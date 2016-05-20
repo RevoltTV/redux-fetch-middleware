1cb929071f7f0f007ed7d9895620481f2584795c
Andrew Burgess <andrew@andrewburgess.io>
2016-05-20 11:13:38 -0700

    test: slight restructure of sections

--- test/index.test.js
+++ test/index.test.js
@@ -74,74 +74,78 @@ describe('redux-fetch-middleware', () => {
         });
     });
 
-    it('should fetch the specified resource', () => {
-        nock('http://localhost').get('/test.json').reply(200, { data: true });
+    describe('basic requests', () => {
+        it('should fetch the specified resource', () => {
+            let request = nock('http://localhost').get('/test.json').reply(200, { data: true });
 
-        // Pending event should fire first, so lets ensure we check both action types
-        let pending = true;
+            // Pending event should fire first, so lets ensure we check both action types
+            let pending = true;
 
-        const middleware = fetchMiddleware()({ getState: doGetState });
-        const actionDefinition = {
-            type: 'test',
-            fetch: {
-                url: 'http://localhost/test.json'
-            }
-        };
+            const middleware = fetchMiddleware()({ getState: doGetState });
+            const actionDefinition = {
+                type: 'test',
+                fetch: {
+                    url: 'http://localhost/test.json'
+                }
+            };
 
-        const actionHandler = middleware((action) => {
-            if (pending) {
-                // The `PENDING` action should be sent first, so ensure that's what we receive, then flip pending so we
-                // can check the next action is the expected type
-                action.type.should.equal(`${actionDefinition.type}_PENDING`);
-                pending = false;
-            } else {
-                // We've already received the `PENDING` action, so now we should receive the real action
-                action.type.should.equal(actionDefinition.type);
-            }
+            const actionHandler = middleware((action) => {
+                if (pending) {
+                    // The `PENDING` action should be sent first, so ensure that's what we receive, then flip pending so we
+                    // can check the next action is the expected type
+                    action.type.should.equal(`${actionDefinition.type}_PENDING`);
+                    pending = false;
+                } else {
+                    // We've already received the `PENDING` action, so now we should receive the real action
+                    action.type.should.equal(actionDefinition.type);
+                }
+            });
+
+            let promise = actionHandler(actionDefinition);
+            return promise.should.be.fulfilled.and.
+                           should.eventually.be.a('object').and.
+                           should.eventually.deep.equal({ data: true }).
+            then(() => {
+                pending.should.equal(false);
+                request.isDone().should.equal(true);
+            });
         });
 
-        let promise = actionHandler(actionDefinition);
-        return promise.should.be.fulfilled.and.
-                       should.eventually.be.a('object').and.
-                       should.eventually.deep.equal({ data: true }).
-        then(() => {
-            pending.should.equal(false);
-        });
-    });
+        it('should send FAILURE action when statusCode is not an acceptable value', () => {
+            let request = nock('http://localhost').get('/test.json').reply(404, 'Not found');
 
-    it('should send FAILURE action when statusCode is not an acceptable value', () => {
-        nock('http://localhost').get('/test.json').reply(404, 'Not found');
+            let pending = true;
 
-        let pending = true;
+            const middleware = fetchMiddleware()({ getState: doGetState });
+            const actionDefinition = {
+                type: 'test',
+                fetch: {
+                    url: 'http://localhost/test.json'
+                }
+            };
 
-        const middleware = fetchMiddleware()({ getState: doGetState });
-        const actionDefinition = {
-            type: 'test',
-            fetch: {
-                url: 'http://localhost/test.json'
-            }
-        };
+            const actionHandler = middleware((action) => {
+                if (pending) {
+                    // The `PENDING` action should be sent first, so ensure that's what we receive, then flip pending so we
+                    // can check the next action is the expected type
+                    action.type.should.equal(`${actionDefinition.type}_PENDING`);
+                    pending = false;
+                } else {
+                    // We've already received the `PENDING` action, so now we should receive the real action
+                    action.type.should.equal(`${actionDefinition.type}_FAILURE`);
+                }
+            });
 
-        const actionHandler = middleware((action) => {
-            if (pending) {
-                // The `PENDING` action should be sent first, so ensure that's what we receive, then flip pending so we
-                // can check the next action is the expected type
-                action.type.should.equal(`${actionDefinition.type}_PENDING`);
-                pending = false;
-            } else {
-                // We've already received the `PENDING` action, so now we should receive the real action
-                action.type.should.equal(`${actionDefinition.type}_FAILURE`);
-            }
-        });
+            let promise = actionHandler(actionDefinition);
 
-        let promise = actionHandler(actionDefinition);
-
-        return promise.should.be.rejected.
-        then((err) => {
-            err.status.should.equal(404);
-            err.response.should.be.a('object');
-            err.response.statusText.should.equal('Not Found');
-            pending.should.equal(false);
+            return promise.should.be.rejected.
+            then((err) => {
+                err.status.should.equal(404);
+                err.response.should.be.a('object');
+                err.response.statusText.should.equal('Not Found');
+                pending.should.equal(false);
+                request.isDone().should.equal(true);
+            });
         });
     });
 });
